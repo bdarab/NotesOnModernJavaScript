ec6c13a7a74800635a3f6547f63a5cd359ef95fe
bdarab <bdarabzand@gmail.com>
2021-08-27 01:31:32 +0200

    Add Clear & Filter Tasks Functionality

--- 08.DOM-PROJECTS/TaskList/app.js
+++ 08.DOM-PROJECTS/TaskList/app.js
@@ -1,18 +1,21 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-arrow-callback */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-alert */
 
-// Define UI Variables
+// DEFINE UI VARIABLES
 const form = document.querySelector('#task-form')
 const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
 
-// Load all event listeners
+// LOAD ALL EVENT LISTENER
 loadEventListeners()
 
-// Load all event listeners function
+// LOAD ALL EVENT LISTENER FUNCTION
 function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask)
@@ -22,10 +25,15 @@ function loadEventListeners(){
   // remove task event
   taskList.addEventListener('click', removeTask)
 
+  // clear tasks event
+  clearBtn.addEventListener('click', clearTasks)
+
+  // filter tasks event
+  filter.addEventListener('keyup', filterTasks)
 }
 
 
-// Add Task Function
+// ADD TASK FUNCTION
 function addTask (e) {
   if (taskInput.value === '') {
     alert('Add a task')
@@ -53,7 +61,7 @@ function addTask (e) {
   e.preventDefault()
 }
 
-// Add removeTask function
+// REMOVE TASK FUNCTION
 function removeTask (e) {
   /* Comment above: the delete element is multiple and dynamic, so we MUST USE EVENT DELEGATION targeting taskList(UL) */
   // must target specific element in taskList(UL), namely the X icon
@@ -62,5 +70,36 @@ function removeTask (e) {
       e.target.parentElement.parentElement.remove()
     }
  }
+  e.preventDefault()
+}
+
+// CLEAR TASKS FUNCTION
+function clearTasks (e) {
+  // setting HTML to empty
+  // taskList.innerHTML = ''
+
+  // faster way is to loop through the items 
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild)
+
+    // ARTICLE: showing the difference between the 2 methods https://coderwall.com/p/nygghw/don-t-use-innerhtml-to-empty-dom-elements
+}
+  e.preventDefault()
+}
+
+// FILTER TASKS FUNCTION
+function filterTasks (e) {
+  const text = e.target.value.toLowerCase()
+
+  /* forEach method can be used because querySelector() returns a NODE LIST while other methods of getElementBy...() will return HTML COLLECTIONS which must be converted to an array in order to use forEach() method */
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  })
+
   e.preventDefault()
 }
